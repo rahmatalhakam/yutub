0ae8e923666b066bc9d6f5f1781f78c930b5c05c
Rahmat Al Hakam <rahmatalhakam@gmail.com>
2019-04-08 06:37:33 +0700

    add photo profile

--- user/User.js
+++ user/User.js
@@ -3,6 +3,10 @@ const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
+  photo_profile: {
+    id_photo: String,
+    photo_url: String
+  },
   created_at: { type: Date, default: Date.now },
   updated_at: Date
 });
--- user/UserController.js
+++ user/UserController.js
@@ -3,13 +3,23 @@ var router = express.Router();
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
 var bodyParser = require("body-parser");
-
+const multer = require("multer");
+const fs = require("fs");
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
+const cloudinary = require("cloudinary");
 var User = require("./User");
 var config = require("../config");
 var VerifyToken = require("../auth/VerifyToken");
+
+cloudinary.config({
+  cloud_name: config.cloud_name,
+  api_key: config.api_key,
+  api_secret: config.api_secret
+});
+
+const parser = multer({ dest: "storage/" });
 
 // create new user
 router.post("/", function(req, res) {
@@ -67,12 +77,21 @@ router.get("/all", function(req, res) {
 
 //get user by token
 router.get("/", VerifyToken, function(req, res, next) {
-  User.findById(req.userId, { password: 0 }, function(err, user) {
-    if (err)
-      return res.status(500).send("There was a problem finding the user.");
-    if (!user) return res.status(404).send("No user found.");
-    res.status(200).send(user);
-  });
+  const { name, email } = req.query;
+  User.find(
+    {
+      _id: req.userId,
+      name: new RegExp(name, "i"),
+      email: new RegExp(email, "i")
+    },
+    { password: 0 },
+    function(err, user) {
+      if (err)
+        return res.status(500).send("There was a problem finding the user.");
+      if (!user) return res.status(404).send("No user found.");
+      res.status(200).send(user);
+    }
+  );
 });
 
 // UPDATES A SINGLE USER IN THE DATABASE
@@ -85,6 +104,33 @@ router.put("/", VerifyToken, function(req, res) {
     if (err)
       return res.status(500).send("There was a problem updating the user.");
     res.status(200).send(user);
+  });
+});
+
+//update photo profile
+router.put("/attachment", [VerifyToken, parser.single("image")], (req, res) => {
+  cloudinary.v2.uploader.upload(req.file.path, (err, result) => {
+    fs.unlink(req.file.path, function(err) {
+      if (err) throw err;
+    });
+    if (err) return result.status(500).send(error);
+    const photo_url = result.secure_url;
+    const id_photo = result.public_id;
+    User.findByIdAndUpdate(
+      req.userId,
+      {
+        $set: {
+          "photo_profile.photo_url": photo_url,
+          "photo_profile.id_photo": id_photo
+        }
+      },
+      { new: true },
+      (err, user) => {
+        if (err)
+          return res.status(500).send("There was a problem updating the user.");
+        res.status(200).send(user);
+      }
+    );
   });
 });
 

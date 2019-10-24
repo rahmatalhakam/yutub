3d1186dc575020262d1e055064d827a68cad9364
Rahmat Aziz Al Hakam <rahmatazizalhakam@Rahmats-MacBook-Pro.local>
2019-10-24 08:41:51 +0700

    [rahmat] update dependencies

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-var mongoose = require("mongoose");
-mongoose.connect("mongodb://yutub:yutub1234@ds119164.mlab.com:19164/yutub", {
-  useNewUrlParser: true
-});
+var mongoose = require('mongoose')
+mongoose.connect('mongodb://yutub:yutub1234@ds119164.mlab.com:19164/yutub', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
